b6916d97ec6c3e7d0925509f9e6ae54602684ac3
Hawks <24587652+hawkinzzz@users.noreply.github.com>
2022-12-10 17:52:01 +0000

    Fix sudo and service checks

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #################################################################
-# Author(s): Hawks                                              #
+# Author: Hawks                                                 #
 # Version: v0.1.0                                               #
 # Date: 2022-12-10                                              #
 # Description:  Google file upload script that uses Rclone      #
@@ -15,10 +15,10 @@ USERAGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, 
 EXCLUDEARRAY=("*.nfo" "*.jpeg" "*.jpg" "*sample*" "*SAMPLE*" "*.png" "*.html" "*.backup~" "*.partical~" "**_HIDDEN~" "*.unionfs/**" "**partial~" ".unionfs-fuse/**" ".fuse_hidden**" ".FUSE_HIDDEN**" "**.grab/**" "**sabnzbd**" "**nzbget**" "**qbittorrent**" "**rutorrent**" "**deluge**" "**transmission**" "**jdownloader**" "**makemkv**" "**handbrake**" "**bazarr**" "**ignore**" "**inProgress**" "**torrent**" "**nzb**")
 SERVICE="/etc/systemd/system/upload.service"
 
-### Creates readable/usable array for Rsync & Rclone command arguments ###
+### Creates readable/usable array for Rsync & Rclone command arguments
 EXCLUDELIST=("${EXCLUDEARRAY[@]/#/--exclude=}")
 
-### Log files ###
+### Log files
 LOGS="$BASE/logs/upload.log"
 VERBOSELOG="$BASE/logs/verbose.log"
 ERRORLOG="$BASE/logs/error.log"
@@ -30,13 +30,14 @@ log() {
 }
 
 sudoCheck() {
-    ### Check if script is running as root for installs ###
-    [[ ! $(id -u) == "0" ]] && log "Script not running with Sudo. Exiting script now"
-    exit 0
+    ### Check if script is running as root
+    if [[ ! $(id -u) == "0" ]]; then 
+        log "Script not running with Sudo. Exiting script now"
+        exit 0
+    fi
 }
 
 installRedis() {
-    ### Redis is used to keep track of key position past script restarts and also allows for manual overide ###
     sudoCheck
     curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg 1>$VERBOSELOG 2>$ERRORLOG
     echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list 1>$VERBOSELOG 2>$ERRORLOG
@@ -45,20 +46,18 @@ installRedis() {
 }
 
 installRsync() {
-    ### Rsync is used to copy over files into a neutral directory before they are uploaded ###
     sudoCheck
     sudo apt-get update 1>$VERBOSELOG 2>$ERRORLOG
     sudo apt-get install rsync 1>$VERBOSELOG 2>$ERRORLOG || exit 1
 }
 
 installRclone() {
-    ### Rclone is used to upload files to Google Drive ###
     sudoCheck
     curl https://rclone.org/install.sh | sudo bash 1>$VERBOSELOG 2>$ERRORLOG || exit 1
 }
 
 installUpload() {
-    ### Install and start upload service and exit manual running script ###
+    ### Install and start upload service and exit manual running script
     sudoCheck
     [[ ! -f "$SERVICE" ]] && sudo tee -a $SERVICE <<EOF
 [Unit]
@@ -85,14 +84,14 @@ EOF
 }
 
 startup() {
-    ## Install apps required for upload script ###
+    ### Make required folder and files for the script ###
+    mkdir -p $BASE/logs /mnt/{downloads,move} &
+    ## Install apps required for upload script
     [[ -z $(command -v redis-cli) ]] && installRedis
     [[ -z $(command -v rsync) ]] && installRsync
     [[ -z $(command -v rclone) ]] && installRclone
-    ### Create and start upload service if it doesn't exist ###
-    [[ ! $(pgrep -x "upload") ]] && installUpload
-    ### Make required folder and files for the script ###
-    mkdir -p $BASE/logs /mnt/{downloads,move}
+    ### Create and start upload service if it doesn't exist
+    service upload status &> /dev/null || installUpload
     ### Clear logs ###
     sleep 5
     : >$LOGS
@@ -100,7 +99,7 @@ startup() {
 }
 
 keyRotate() {
-    ### If the uploader gets stuck, restarting the service will automatically choose the next key ###
+    ### If the uploader gets stuck, restarting the script will automatically choose the next key ###
     if [[ -z "$(redis-cli GET liveKey)" || "$(redis-cli GET liveKey)" -gt $((KEYS - 1)) ]]; then
         redis-cli SET liveKey 1
         sa="GDSA$(redis-cli --raw GET liveKey)C"
@@ -125,7 +124,7 @@ fileWait() {
 }
 
 upload() {
-    ### Once files exist in /mnt/move start the upload command ###
+    ### Once files exist in /mnt/move start the upload script ###
     log "Found files to upload, starting upload"
     log "Starting upload with $sa"
     rclone moveto "/mnt/move" "${sa}:/" \
@@ -152,7 +151,7 @@ upload() {
 }
 
 cleanDownloads() {
-    ### Print all file paths in folder to a temp file, then delete if they exist 24 hours later to keep clean ###
+    ### Print all file paths in folder to a temp file, then delete if they exist 24 hours later to keep clean
     find /mnt/downloads/nzb/ -type f >/dev/shm/files2Clean
     sleep $CLEANSLEEP
     log "Cleaning files in the download folder"
